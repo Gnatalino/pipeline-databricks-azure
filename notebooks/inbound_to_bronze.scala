d213446d965af783eda3e921803e22471e059f0c
gnatalinosouza@gmail.com <gnatalinosouza@gmail.com>
2024-04-24 00:58:27 +0000

    notebooks do pipeline

---
+++ notebooks/inbound_to_bronze.scala
@@ -0,0 +1,36 @@
+// Databricks notebook source
+// MAGIC %python
+// MAGIC dbutils.fs.ls("/mnt/dados/inbound")
+
+// COMMAND ----------
+
+// Lendo dados da camanda inbound 
+val path = "dbfs:/mnt/dados/inbound/dados_brutos_imoveis.json"
+val dados = spark.read.json(path)
+
+// COMMAND ----------
+
+display(dados)
+
+// COMMAND ----------
+
+// Removendo colunas
+val dados_anuncio = dados.drop("imagens", "usuario")
+display(dados_anuncio)
+
+// COMMAND ----------
+
+// Criando uma coluna de identificação
+import org.apache.spark.sql.functions.col
+val df_bronze = dados_anuncio.withColumn("id", col("anuncio.id"))
+display(df_bronze)
+
+// COMMAND ----------
+
+val path = "dbfs:/mnt/dados/bronze/dataset_imoveis"
+df_bronze.write.format("delta").mode(SaveMode.Overwrite).save(path)
+
+// COMMAND ----------
+
+// MAGIC %python
+// MAGIC dbutils.fs.ls("/mnt/dados/bronze/")
